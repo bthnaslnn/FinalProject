d3ed120f3447ef677649fca8b7d6123b523b79c4
Cİvohan <batuhan.aslanbm@gmail.com>
2025-07-06 20:37:58 +0300

    EfEntityRepositoryBase and Dto created and in program.cs , this actions tested.

---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,65 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity,TContext>:IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()   
+    {
+        public void Add(TEntity entity)
+        {
+            //IDısposible pattern implementation of c#
+            using (TContext context = new TContext())
+            {
+                var addedEntity = context.Entry(entity); //Git verikaynağından benim gönderdiğim entity'E eşleştir. Ekleme olduğu için direkt ekliyor.
+                addedEntity.State = EntityState.Added; //Veritabanında ekle demek.
+                context.SaveChanges(); //Veritabanındaki değişiklikler .
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity); //Git verikaynağından benim gönderdiğim entity'E eşleştir. Ekleme olduğu için direkt ekliyor.
+                deletedEntity.State = EntityState.Deleted; //Veritabanında sil demek. Durumu silinicek.
+                context.SaveChanges(); //Veritabanındaki değişiklikler .
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                return filter == null
+                    ? context.Set<TEntity>().ToList()
+                    : context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var uptadedEntity = context.Entry(entity); //Git verikaynağından benim gönderdiğim entity'E eşleştir. Ekleme olduğu için direkt ekliyor.
+                uptadedEntity.State = EntityState.Modified; //Veritabanında güncelleme demek. Durumu güncellenecek.
+                context.SaveChanges(); //Veritabanındaki değişiklikler .
+            }
+        }
+    }
+}
--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,5 +1,7 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -11,55 +13,25 @@ using System.Threading.Tasks;
 namespace DataAccess.Concrete.EntityFramework
 {
     //NuGet
-    public class EfProductDal : IProductDal
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
     {
-        public void Add(Product entity)
-        {
-            //IDısposible pattern implementation of c#
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                var addedEntity = context.Entry(entity); //Git verikaynağından benim gönderdiğim entity'E eşleştir. Ekleme olduğu için direkt ekliyor.
-                addedEntity.State = EntityState.Added; //Veritabanında ekle demek.
-                context.SaveChanges(); //Veritabanındaki değişiklikler .
-            }
-        }
-
-        public void Delete(Product entity)
+        public List<ProductDetailDto> GetProductDetails()
         {
             using (NorthwindContext context = new NorthwindContext())
             {
-                var deletedEntity = context.Entry(entity); //Git verikaynağından benim gönderdiğim entity'E eşleştir. Ekleme olduğu için direkt ekliyor.
-                deletedEntity.State = EntityState.Deleted; //Veritabanında sil demek. Durumu silinicek.
-                context.SaveChanges(); //Veritabanındaki değişiklikler .
+                var result = from p in context.Products
+                             join c in context.Categories
+                             on p.CategoryId equals c.CategoryId
+                             select new ProductDetailDto
+                             {
+                                 ProductId = p.ProductId,
+                                 ProductName = p.ProductName,
+                                 CategoryName = c.CategoryName,
+                                 UnitsInStock = p.UnitsInStock
+                             };
+                return result.ToList(); 
             }
-        }
-
-        public Product Get(Expression<Func<Product, bool>> filter)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                return context.Set<Product>().SingleOrDefault(filter);
-            }
-        }
-
-        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                return filter == null 
-                    ? context.Set<Product>().ToList() 
-                    : context.Set<Product>().Where(filter).ToList();
-            }
-        }
-
-        public void Update(Product entity)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                var uptadedEntity = context.Entry(entity); //Git verikaynağından benim gönderdiğim entity'E eşleştir. Ekleme olduğu için direkt ekliyor.
-                uptadedEntity.State = EntityState.Modified; //Veritabanında güncelleme demek. Durumu güncellenecek.
-                context.SaveChanges(); //Veritabanındaki değişiklikler .
-            }
+            
         }
     }
 }
